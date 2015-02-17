e9fd3ae1a2224db69f4d96165676e9c7688de9a3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2015-02-17 14:03:25 +0100

    Test : inject gulp for future dev

--- test/angular-architecture-graph.test.js
+++ test/angular-architecture-graph.test.js
@@ -1,10 +1,16 @@
 define([
     'intern/chai!expect',
     'intern!bdd',
-    'intern/dojo/node!../index.js'
-], function(expect, bdd, ngArchitectureGraph) {
+    'intern/dojo/node!../index.js',
+    'intern/dojo/node!path',
+    'intern/dojo/node!gulp'
+], function(expect, bdd, ngArchitectureGraph, path, gulp) {
 
     bdd.describe('ngArchitectureGraph plugin', function() {
+
+        var stream = ngArchitectureGraph();
+
+        //console.log(stream);
 
         bdd.it('should do the job', function() {
             expect(1).to.equal(1);
